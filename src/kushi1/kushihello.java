b776ffad5bdd9eed38cb96d38eda346e6cc8a6a3
murali <murali@murali-PC.attlocal.net>
2021-10-25 15:16:34 -0700

    all class updated

---
+++ src/kushi1/kushihello.java
@@ -0,0 +1,10 @@
+package kushi1;
+
+public class kushihello {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to kushi");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module KushiProject {
+}
